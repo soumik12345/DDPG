c871e2cb0e6268282679d3366a436437511f55af
soumik12345 <19soumik.rakshit96@gmail.com>
2020-03-17 12:29:36 +0530

    Added results for mountain car

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ from test import Tester
 #     './pretrained_models/lunar_lander_continuous_v2/ddpg_lunarlander_v2_0'
 # )
 
+# tester = Tester(
+#     './configs/BipedalWalkerHardcore-v3.json',
+#     './pretrained_models/bipedal_walker_hardcore_v3/ddpg_bipedalwalker_hardcore_v3_0'
+# )
+
 tester = Tester(
-    './configs/BipedalWalkerHardcore-v3.json',
-    './pretrained_models/bipedal_walker_hardcore_v3/ddpg_bipedalwalker_hardcore_v3_0'
+    './configs/MountainCarContinuous-v0.json',
+    './pretrained_models/mountain_car_v0/ddpg_mountaincar_v0_0'
 )
 
-print('Mean Reward:', tester.test(eval_episodes=20, render=True))
+print('Mean Reward:', tester.test(eval_episodes=20, render=False))
